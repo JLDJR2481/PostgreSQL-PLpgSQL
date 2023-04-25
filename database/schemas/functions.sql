7d0d73b29f1bfa0caa3dd734e32d3824b343e34e
ncocana <ncocana@cifpfbmoll.eu>
2023-04-25 22:06:25 +0200

    fix(functions): add missing semicolon and indentantion

--- database/schemas/functions.sql
+++ database/schemas/functions.sql
@@ -10,36 +10,44 @@
 -- -----------------------------------------------------
 DROP FUNCTION IF EXISTS get_large_airlines_name;
 
-CREATE FUNCTION get_large_airlines_name(min_employees INT)
-RETURNS TABLE (large_airline_name VARCHAR(100))
+CREATE OR REPLACE FUNCTION get_large_airlines_name(
+    min_employees INT
+)
+RETURNS TABLE (
+    large_airline_name VARCHAR(100)
+)
 LANGUAGE plpgsql
 AS $$
 BEGIN
-RETURN QUERY
-SELECT name
-FROM airlines
-WHERE num_emp >= min_employees;
-END;
-$$;
+    RETURN QUERY
+    SELECT name
+    FROM airlines
+    WHERE num_emp >= min_employees;
+END;$$;
 
 -- To prove it --
-SELECT * FROM get_large_airlines_name(400)
+SELECT * FROM get_large_airlines_name(400);
 
 
 DROP FUNCTION IF EXISTS get_larger_airlines_data;
 
-CREATE FUNCTION get_larger_airlines_data(min_employees INT)
-RETURNS TABLE (larger_airline VARCHAR(100), total_employees INT)
+CREATE OR REPLACE FUNCTION get_larger_airlines_data(
+    min_employees INT
+)
+RETURNS TABLE (
+    larger_airline VARCHAR(100),
+    total_employees INT
+)
 LANGUAGE plpgsql
 AS $$
 DECLARE
-airline_record record;
+    airline_record record;
 BEGIN 
-FOR airline_record IN (SELECT * FROM airlines WHERE num_emp >= min_employees)
-LOOP
-RETURN QUERY SELECT airline_record.name, airline_record.num_emp;
-END LOOP;
-RETURN;
+    FOR airline_record IN (SELECT * FROM airlines WHERE num_emp >= min_employees)
+    LOOP
+        RETURN QUERY SELECT airline_record.name, airline_record.num_emp;
+        END LOOP;
+    RETURN;
 END;
 $$;
 
